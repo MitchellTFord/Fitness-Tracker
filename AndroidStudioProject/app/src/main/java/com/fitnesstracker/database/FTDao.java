2931b86ba06090096c5d79958c7bd7cf28522077
Mitchell Ford <mitchelltford@gmail.com>
2020-09-30 19:04:10 -0500

    Finished the design of the Food database schema

--- AndroidStudioProject/app/src/main/java/com/fitnesstracker/database/FTDao.java
+++ AndroidStudioProject/app/src/main/java/com/fitnesstracker/database/FTDao.java
@@ -1,12 +1,12 @@
 package com.fitnesstracker.database;
 
+import androidx.annotation.Nullable;
 import androidx.room.Dao;
 import androidx.room.Insert;
 import androidx.room.OnConflictStrategy;
 import androidx.room.Query;
 
 import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
 
 import java.util.List;
 
@@ -22,14 +22,15 @@ public interface FTDao
      * Query the database for a Food with a given id.
      *
      * @param id the id of the Food to search for
+     *
      * @return the Food with the given id, or null if no such Food exists
      */
     @Nullable
     @Query("SELECT * FROM Food WHERE Food.id = :id")
-    public Food getFood(@NotNull long id);
+    Food getFood(long id);
 
     @Query("SELECT * FROM Food WHERE Food.name LIKE :name")
-    public List<Food> getFood(@NotNull String name);
+    List<Food> getFood(@NotNull String name);
 
     /**
      * Query the database for a {@link java.util.List} of all the Foods it has.
@@ -37,7 +38,7 @@ public interface FTDao
      * @return a {@link List} of all the Foods in the database
      */
     @Query("SELECT * FROM Food")
-    public List<Food> getAllFoods();
+    List<Food> getAllFoods();
 
     /**
      * Insert Food objects into the database.
@@ -45,5 +46,5 @@ public interface FTDao
      * @param foods one or more Food objects to insert into the database
      */
     @Insert(onConflict = OnConflictStrategy.REPLACE)
-    public void insert(@NotNull Food... foods);
+    void insert(@NotNull Food... foods);
 }
