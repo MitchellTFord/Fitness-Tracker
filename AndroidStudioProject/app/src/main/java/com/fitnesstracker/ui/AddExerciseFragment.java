d787d6407f3bee8057d3d7d4978132d2f2fd6d62
alyssadmoore <alyssadmoore@gmail.com>
2020-10-07 20:04:38 -0500

    add the AddExercise button to Exercise page

---
+++ AndroidStudioProject/app/src/main/java/com/fitnesstracker/ui/AddExerciseFragment.java
@@ -0,0 +1,46 @@
+package com.fitnesstracker.ui;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+import com.fitnesstracker.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link AddExerciseFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class AddExerciseFragment extends Fragment {
+
+    public AddExerciseFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @return A new instance of fragment AddExerciseFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static AddExerciseFragment newInstance() {
+        AddExerciseFragment fragment = new AddExerciseFragment();
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_add_food, container, false);
+    }
+}
