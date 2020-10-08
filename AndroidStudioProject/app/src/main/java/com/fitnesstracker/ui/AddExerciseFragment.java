1503eeae76e35e35c71ee8c001b2db76f096ca70
alyssadmoore <alyssadmoore@gmail.com>
2020-10-07 20:08:03 -0500

    Update AddExerciseFragment.java

--- AndroidStudioProject/app/src/main/java/com/fitnesstracker/ui/AddExerciseFragment.java
+++ AndroidStudioProject/app/src/main/java/com/fitnesstracker/ui/AddExerciseFragment.java
@@ -41,6 +41,6 @@ public class AddExerciseFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_add_food, container, false);
+        return inflater.inflate(R.layout.fragment_add_exercise, container, false);
     }
 }
