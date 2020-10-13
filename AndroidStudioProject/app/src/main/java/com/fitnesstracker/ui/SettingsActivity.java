49847e3e9360700ab6557d6f57edcc28ae762e6d
Mitchell Ford <mitchelltford@gmail.com>
2020-10-13 11:52:39 -0500

    Added confirmation dialog when clearing database

--- AndroidStudioProject/app/src/main/java/com/fitnesstracker/ui/SettingsActivity.java
+++ AndroidStudioProject/app/src/main/java/com/fitnesstracker/ui/SettingsActivity.java
@@ -1,9 +1,11 @@
 package com.fitnesstracker.ui;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.widget.Toast;
 
 import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.ViewModelProviders;
 import androidx.preference.Preference;
@@ -44,8 +46,23 @@ public class SettingsActivity extends AppCompatActivity {
 			Preference clearDataBase = findPreference("clear_db");
 			clearDataBase.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
 				@Override public boolean onPreferenceClick(Preference preference) {
-					viewModel.clearAllTables();
-					Toast.makeText(requireContext(), getString(R.string.clear_db_message), Toast.LENGTH_SHORT).show();
+					// Open a dialog box for the user to confirm their choice
+					new AlertDialog.Builder(requireContext())
+							.setTitle(R.string.clear_db_title)
+							.setMessage(R.string.clear_db_confirmation_dialog)
+
+							// Clear all DB tables when the user presses "yes"
+							.setPositiveButton(R.string.yes, new DialogInterface.OnClickListener() {
+								@Override public void onClick(DialogInterface dialog, int which) {
+									viewModel.clearAllTables();
+									Toast.makeText(requireContext(), R.string.clear_db_message, Toast.LENGTH_SHORT).show();
+								}
+							})
+
+							// Do nothing when the user presses "no"
+							.setNegativeButton(R.string.no, null)
+							.setIcon(R.drawable.ic_warning)
+							.show();
 					return true;
 				}
 			});
