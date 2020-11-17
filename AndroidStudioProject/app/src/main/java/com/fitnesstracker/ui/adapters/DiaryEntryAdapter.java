2ea4a6a0807b4875237777dae651e3393dcd80ed
Mitchell Ford <mitchelltford@gmail.com>
2020-11-17 10:16:04 -0600

    Modified DiaryEntryAdapter to optionally use a OnItemClickListener

--- AndroidStudioProject/app/src/main/java/com/fitnesstracker/ui/adapters/DiaryEntryAdapter.java
+++ AndroidStudioProject/app/src/main/java/com/fitnesstracker/ui/adapters/DiaryEntryAdapter.java
@@ -7,10 +7,12 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.fitnesstracker.R;
 import com.fitnesstracker.database.Meal;
+import com.fitnesstracker.database.entities.FoodDiaryEntry;
 
 import java.text.DateFormat;
 import java.util.ArrayList;
@@ -20,7 +22,37 @@ import java.util.Locale;
 
 public class DiaryEntryAdapter extends RecyclerView.Adapter<DiaryEntryAdapter.ViewHolder> {
 
+	/**
+	 * The data that the {@link RecyclerView} should display.
+	 */
 	private List<Meal> data = new ArrayList<>();
+
+	/**
+	 * The listener for clicks and long-clicks on view holders in this adapter.
+	 * <p>
+	 * This field may be null.
+	 * <p>
+	 * This field must be specified in a constructor.
+	 */
+	@Nullable
+	private final OnItemClickListener<Meal> onItemClickListener;
+
+	/**
+	 * Default constructor that does not specify {@link DiaryEntryAdapter onItemClickListener}.
+	 */
+	public DiaryEntryAdapter() {
+		this(null);
+	}
+
+	/**
+	 * Constructor that specifies {@link DiaryEntryAdapter onItemClickListener}.
+	 *
+	 * @param onItemClickListener the listener that should be used for clicks and long-clicks in
+	 *                            this adapter
+	 */
+	public DiaryEntryAdapter(@Nullable OnItemClickListener<Meal> onItemClickListener) {
+		this.onItemClickListener = onItemClickListener;
+	}
 
 	@NonNull
 	@Override
@@ -48,14 +80,19 @@ public class DiaryEntryAdapter extends RecyclerView.Adapter<DiaryEntryAdapter.Vi
 		holder.timeInfoText.setText(DateFormat
 				.getTimeInstance(DateFormat.SHORT)
 				.format(new Date(meal.getFoodDiaryEntry().getTime())));
+
+		// Notify onItemClickListener if it has been set
+		if (onItemClickListener != null) {
+			holder.bind(meal, onItemClickListener);
+		}
 	}
 
 	@Override
 	public int getItemCount() {
-		return data.size();
+		return data == null ? 0 : data.size();
 	}
 
-	public void setData(List<Meal> data) {
+	public void setData(@Nullable List<Meal> data) {
 		this.data = data;
 		notifyDataSetChanged();
 	}
@@ -70,6 +107,27 @@ public class DiaryEntryAdapter extends RecyclerView.Adapter<DiaryEntryAdapter.Vi
 
 			foodInfoText = itemView.findViewById(R.id.food_info_text);
 			timeInfoText = itemView.findViewById(R.id.time_info_text);
+		}
+
+		/**
+		 * Bind this {@link ViewHolder} to an {@link OnItemClickListener} to handle click events.
+		 *
+		 * @param meal                the {@link Meal} object held by this view holder
+		 * @param onItemClickListener the {@link OnItemClickListener} that should handle click
+		 *                            events for this view holder
+		 */
+		public void bind(final Meal meal, final OnItemClickListener<Meal> onItemClickListener) {
+			itemView.setOnClickListener(new View.OnClickListener() {
+				@Override public void onClick(View v) {
+					onItemClickListener.onItemClicked(meal);
+				}
+			});
+			itemView.setOnLongClickListener(new View.OnLongClickListener() {
+				@Override public boolean onLongClick(View v) {
+					onItemClickListener.onItemLongClicked(meal);
+					return true;
+				}
+			});
 		}
 	}
 }
