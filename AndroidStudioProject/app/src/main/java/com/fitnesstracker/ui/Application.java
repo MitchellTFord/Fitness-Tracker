553cdbd282b5fcd04a822cd283e2b82dc61ae142
Mitchell Ford <mitchelltford@gmail.com>
2020-11-19 19:55:34 -0600

    Wrote createNotificationChannel() in Application

--- AndroidStudioProject/app/src/main/java/com/fitnesstracker/ui/Application.java
+++ AndroidStudioProject/app/src/main/java/com/fitnesstracker/ui/Application.java
@@ -2,6 +2,8 @@ package com.fitnesstracker.ui;
 
 import android.app.Notification;
 import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.os.Build;
 import android.util.Log;
 
 public class Application extends android.app.Application {
@@ -13,12 +15,27 @@ public class Application extends android.app.Application {
 	public void onCreate() {
 		super.onCreate();
 
-		Log.d(TAG, "Application onCreate()");
+		Log.d(TAG, "onCreate()");
 
-
+		createNotificationChannel();
 	}
 
+	/**
+	 * Initialize the notification channels used by this app.
+	 */
 	private void createNotificationChannel() {
 
+		// Create notification channels only on 26+
+		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+			CharSequence name = "Reminders";
+			String description = "Reminders to create diary entries";
+			int importance = NotificationManager.IMPORTANCE_DEFAULT;
+
+			NotificationChannel channel = new NotificationChannel(REMINDER_CHANNEL_ID, name, importance);
+			channel.setDescription(description);
+
+			NotificationManager notificationManager = getSystemService(NotificationManager.class);
+			notificationManager.createNotificationChannel(channel);
+		}
 	}
 }
