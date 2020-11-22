48815bae7a8a484d60ce05b51f8d9a0be3937a43
Mitchell Ford <mitchelltford@gmail.com>
2020-11-21 20:17:35 -0600

    Created NotificationPublisher

---
+++ AndroidStudioProject/app/src/main/java/com/fitnesstracker/ui/notifications/NotificationPublisher.java
@@ -0,0 +1,16 @@
+package com.fitnesstracker.ui.notifications;
+
+import android.app.job.JobScheduler;
+import android.app.job.JobService;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Handler;
+
+public class NotificationPublisher extends BroadcastReceiver {
+
+	@Override
+	public void onReceive(Context context, Intent intent) {
+
+	}
+}
