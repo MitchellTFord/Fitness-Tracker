cc76fe8ee667e29067f0a6767676663c9d35badd
Mitchell Ford <mitchelltford@gmail.com>
2020-11-21 20:23:53 -0600

    Set up NotificationPublisher as a BroadcastReceiver

--- AndroidStudioProject/app/src/main/java/com/fitnesstracker/ui/notifications/NotificationPublisher.java
+++ AndroidStudioProject/app/src/main/java/com/fitnesstracker/ui/notifications/NotificationPublisher.java
@@ -1,16 +1,29 @@
 package com.fitnesstracker.ui.notifications;
 
-import android.app.job.JobScheduler;
-import android.app.job.JobService;
+import android.app.Notification;
+import android.app.NotificationManager;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.os.Handler;
 
 public class NotificationPublisher extends BroadcastReceiver {
+
+	public static final String KEY_NOTIFICATION_ID = "notification-id";
+	public static final String KEY_NOTIFICATION = "notification";
 
 	@Override
 	public void onReceive(Context context, Intent intent) {
 
+		// Get the NotificationManager system service
+		NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+
+		// Get the notification from the intent
+		Notification notification = intent.getParcelableExtra(KEY_NOTIFICATION);
+
+		// Get the notification id from the intent
+		int id = intent.getIntExtra(KEY_NOTIFICATION_ID, 0);
+
+		// Send the notification to the user
+		notificationManager.notify(id, notification);
 	}
 }
