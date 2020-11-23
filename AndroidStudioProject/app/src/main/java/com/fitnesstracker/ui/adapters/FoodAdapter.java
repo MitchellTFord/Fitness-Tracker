4ac0885fd0b880395678803c053ada70e3d38a2f
Mitchell Ford <mitchelltford@gmail.com>
2020-11-22 23:30:54 -0600

    Added some documentation for FoodAdapter

--- AndroidStudioProject/app/src/main/java/com/fitnesstracker/ui/adapters/FoodAdapter.java
+++ AndroidStudioProject/app/src/main/java/com/fitnesstracker/ui/adapters/FoodAdapter.java
@@ -11,14 +11,21 @@ import androidx.annotation.Nullable;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.fitnesstracker.R;
-import com.fitnesstracker.database.Meal;
 import com.fitnesstracker.database.entities.Food;
 
 import java.util.List;
 import java.util.Locale;
 
+/**
+ * A {@link RecyclerView.Adapter} for {@link Food} objects.
+ *
+ * @author Mitchell Ford
+ */
 public class FoodAdapter extends RecyclerView.Adapter<FoodAdapter.ViewHolder> {
 
+	/**
+	 * The data being displayed in recycler views using this adapter.
+	 */
 	private List<Food> data;
 
 	/**
@@ -54,19 +61,23 @@ public class FoodAdapter extends RecyclerView.Adapter<FoodAdapter.ViewHolder> {
 		Context context = parent.getContext();
 		LayoutInflater inflater = LayoutInflater.from(context);
 
+		// Inflate the view holder layout
 		View foodView = inflater.inflate(R.layout.food_view, parent, false);
 
-		ViewHolder viewHolder = new ViewHolder(foodView);
-		return viewHolder;
+		return new ViewHolder(foodView);
 	}
 
 	@Override
 	public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+
+		// Get the food item from data
 		Food food = data.get(position);
 
+		// Set up the name text view
 		TextView nameTextView = holder.nameTextView;
 		nameTextView.setText(food.getName());
 
+		// Set up the serving info text view
 		TextView servingInfoTextView = holder.servingInfoTextView;
 		servingInfoTextView.setText(String.format(Locale.getDefault(),
 				"%.2f %s",
@@ -74,27 +85,59 @@ public class FoodAdapter extends RecyclerView.Adapter<FoodAdapter.ViewHolder> {
 				food.getServingUnit())
 		);
 
-		// Notify onItemClickListener if it has been set
+		// Bind the onItemClickListener if one was given in the constructor
 		if (onItemClickListener != null) {
-			holder.bind(food, onItemClickListener);
+			holder.bindOnItemClickListener(food, onItemClickListener);
 		}
 	}
 
+	/**
+	 * Get the total number of items in this adapter.
+	 * <p>
+	 * If {@link FoodAdapter#data} is <code>null</code>, the item count is considered
+	 * <code>0</code>.
+	 *
+	 * @return the total number of items in this adapter
+	 */
 	@Override
 	public int getItemCount() {
 		return data == null ? 0 : data.size();
 	}
 
+	/**
+	 * Set the data in this adapter.
+	 * <p>
+	 * This method notifies of data changes using {@link RecyclerView.Adapter#notifyDataSetChanged()}.
+	 *
+	 * @param data the new data to set
+	 */
 	public void setData(List<Food> data) {
 		this.data = data;
+
+		// This is a very inefficient way of handling data changes
 		notifyDataSetChanged();
 	}
 
+	/**
+	 * A view holder for {@link Food} objects.
+	 */
 	public static class ViewHolder extends RecyclerView.ViewHolder {
 
+		/**
+		 * Text view that displays the name of this food.
+		 */
 		public TextView nameTextView;
+
+		/**
+		 * Text view that displays this foods serving size and serving unit.
+		 */
 		public TextView servingInfoTextView;
 
+		/**
+		 * Constructor that initializes <code>nameTextView</code> and <code>servingInfoTextView</code>.
+		 *
+		 * @param itemView the view that
+		 */
 		public ViewHolder(@NonNull View itemView) {
 			super(itemView);
 
@@ -110,12 +153,16 @@ public class FoodAdapter extends RecyclerView.Adapter<FoodAdapter.ViewHolder> {
 		 * @param onItemClickListener the {@link OnItemClickListener} that should handle click
 		 *                            events for this view holder
 		 */
-		public void bind(final Food food, final OnItemClickListener<Food> onItemClickListener) {
+		public void bindOnItemClickListener(final Food food, final OnItemClickListener<Food> onItemClickListener) {
+
+			// Set the click listener for this item view
 			itemView.setOnClickListener(new View.OnClickListener() {
 				@Override public void onClick(View v) {
 					onItemClickListener.onItemClicked(food);
 				}
 			});
+
+			// Set the long-click listener for this item view
 			itemView.setOnLongClickListener(new View.OnLongClickListener() {
 				@Override public boolean onLongClick(View v) {
 					onItemClickListener.onItemLongClicked(food);
