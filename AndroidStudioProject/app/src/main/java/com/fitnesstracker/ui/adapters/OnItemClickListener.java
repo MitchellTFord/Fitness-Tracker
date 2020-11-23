4dcc6ce59c1aef1cbd2340e7164561c1b610b8b1
Mitchell Ford <mitchelltford@gmail.com>
2020-11-22 23:20:47 -0600

    Added some documentation to OnItemClickListener

--- AndroidStudioProject/app/src/main/java/com/fitnesstracker/ui/adapters/OnItemClickListener.java
+++ AndroidStudioProject/app/src/main/java/com/fitnesstracker/ui/adapters/OnItemClickListener.java
@@ -6,7 +6,18 @@ package com.fitnesstracker.ui.adapters;
  * @param <T> the type of the data being displayed in the recyclerview
  */
 public interface OnItemClickListener<T> {
+
+	/**
+	 * Handle the clicking of items in a {@link androidx.recyclerview.widget.RecyclerView}.
+	 *
+	 * @param item the item that was clicked
+	 */
 	void onItemClicked(T item);
 
+	/**
+	 * Handle the long-clicking of items in a {@link androidx.recyclerview.widget.RecyclerView}.
+	 *
+	 * @param item the item that was long-clicked
+	 */
 	void onItemLongClicked(T item);
 }
