bc2215ae50b1fd82b51d8ab6aa324da40aacb9e3
Mitchell Ford <mitchelltford@gmail.com>
2020-11-23 08:57:15 -0600

    Added documentation to FoodDao and FoodDiaryEntryDao

--- AndroidStudioProject/app/src/main/java/com/fitnesstracker/database/daos/FoodDao.java
+++ AndroidStudioProject/app/src/main/java/com/fitnesstracker/database/daos/FoodDao.java
@@ -1,5 +1,6 @@
 package com.fitnesstracker.database.daos;
 
+import androidx.annotation.Nullable;
 import androidx.lifecycle.LiveData;
 import androidx.room.Dao;
 import androidx.room.Query;
@@ -11,31 +12,88 @@ import java.util.List;
 /**
  * A data access object for {@link Food} entities to be used with {@link
  * com.fitnesstracker.database.FTDatabase}.
+ * <p>
+ * For every method there is a {@link LiveData}-returning method with the same name except for
+ * having "LD" at the end. For example, if you wish to observe the results of {@link
+ * FoodDao#getAll()}, use {@link FoodDao#getAllLD()}.
  */
 @Dao
 public abstract class FoodDao extends FTDao<Food> {
 
+	/**
+	 * Get a list of all foods.
+	 *
+	 * @return a list of all foods
+	 */
 	@Query("SELECT * FROM food")
 	public abstract List<Food> getAll();
 
+	/**
+	 * Get an observable list of all foods.
+	 *
+	 * @return an observable list of all foods
+	 */
 	@Query("SELECT * FROM food")
 	public abstract LiveData<List<Food>> getAllLD();
 
+	/**
+	 * Get a food by ID.
+	 * <p>
+	 * This method will return null if there is no food with the given ID.
+	 *
+	 * @param id the ID to search for
+	 *
+	 * @return a food with a matching ID or null if there is none
+	 */
+	@Nullable
 	@Query("SELECT * FROM food WHERE id = :id")
 	public abstract Food get(long id);
 
+	/**
+	 * Get an observable food by ID.
+	 * <p>
+	 * This method will not return null, but the returned {@link LiveData} may contain null.
+	 *
+	 * @param id the ID to search for
+	 *
+	 * @return an observable food with a matching ID
+	 */
 	@Query("SELECT * FROM food WHERE id = :id")
 	public abstract LiveData<Food> getLD(long id);
 
+	/**
+	 * Get a list of foods by name.
+	 *
+	 * @param name the name to search for
+	 *
+	 * @return a list of foods a the matching name
+	 */
 	@Query("SELECT * FROM food WHERE name LIKE :name")
 	public abstract List<Food> get(String name);
 
+	/**
+	 * Get an observable list of foods by name.
+	 *
+	 * @param name the name to search for
+	 *
+	 * @return an observable list of foods by name
+	 */
 	@Query("SELECT * FROM food WHERE name LIKE :name")
 	public abstract LiveData<List<Food>> getLD(String name);
 
+	/**
+	 * Get the number of foods.
+	 *
+	 * @return the number of foods
+	 */
 	@Query("SELECT COUNT(id) FROM food")
 	public abstract Integer getCount();
 
+	/**
+	 * Get an observable number of foods
+	 *
+	 * @return an observable number of food
+	 */
 	@Query("SELECT COUNT(id) FROM food")
 	public abstract LiveData<Integer> getCountLD();
 }
--- AndroidStudioProject/app/src/main/java/com/fitnesstracker/database/daos/FoodDiaryEntryDao.java
+++ AndroidStudioProject/app/src/main/java/com/fitnesstracker/database/daos/FoodDiaryEntryDao.java
@@ -1,5 +1,6 @@
 package com.fitnesstracker.database.daos;
 
+import androidx.annotation.Nullable;
 import androidx.lifecycle.LiveData;
 import androidx.room.Dao;
 import androidx.room.Query;
@@ -15,46 +16,126 @@ import java.util.List;
  * com.fitnesstracker.database.FTDatabase}.
  * <p>
  * This class also contains methods for getting {@link Meal} objects from the database.
+ * <p>
+ * In some cases, it may be convenient to update a food diary entry indirectly using a meal. See
+ * {@link FoodDiaryEntryDao#update(Meal...)}.
+ * <p>
+ * For every method there is a {@link LiveData}-returning method with the same name except for
+ * having "LD" at the end. For example, if you wish to observe the results of {@link
+ * FoodDiaryEntryDao#getAll()}, use {@link FoodDiaryEntryDao#getAllLD()}.
  */
 @Dao
 public abstract class FoodDiaryEntryDao extends FTDao<FoodDiaryEntry> {
 
+	/**
+	 * Get a list of all food diary entries.
+	 * <p>
+	 * To load accompanying {@link com.fitnesstracker.database.entities.Food} objects as well, use
+	 * {@link FoodDiaryEntryDao#getAllMeals()}.
+	 *
+	 * @return a list of all food diary entries
+	 */
 	@Query("SELECT * FROM diary_food ORDER BY time DESC")
 	public abstract List<FoodDiaryEntry> getAll();
 
+	/**
+	 * Get an observable list of all food diary entries.
+	 * <p>
+	 * To load accompanying {@link com.fitnesstracker.database.entities.Food} objects as well, use
+	 * {@link FoodDiaryEntryDao#getAllMealsLD()}.
+	 *
+	 * @return and observable list of all food diary entries
+	 */
 	@Query("SELECT * FROM diary_food ORDER BY time DESC")
 	public abstract LiveData<List<FoodDiaryEntry>> getAllLD();
 
+	/**
+	 * Get a food diary entry by ID.
+	 * <p>
+	 * This method will return null if there is no food diary entry with the given ID.
+	 *
+	 * @param id the ID to search for
+	 *
+	 * @return a food diary entry with a matching ID or null if there is none
+	 */
+	@Nullable
 	@Query("SELECT * FROM diary_food WHERE id = :id")
 	public abstract FoodDiaryEntry get(long id);
 
+	/**
+	 * Get an observable food diary entry by ID.
+	 *
+	 * @param id the ID to search for
+	 *
+	 * @return an observable food diary entry
+	 */
 	@Query("SELECT * FROM diary_food WHERE id = :id")
 	public abstract LiveData<FoodDiaryEntry> getLD(long id);
 
+	/**
+	 * Get a food diary entry and it's accompanying {@link com.fitnesstracker.database.entities.Food}
+	 * as a {@link Meal} by ID.
+	 * <p>
+	 * This method will return null if there is no food diary entry with the given ID.
+	 *
+	 * @param foodDiaryEntryId the ID to search for
+	 *
+	 * @return a meal with a matching ID or null if there is none
+	 */
 	@Transaction
+	@Nullable
 	@Query("SELECT * FROM diary_food WHERE id = :foodDiaryEntryId")
 	public abstract Meal getMeal(Long foodDiaryEntryId);
 
+	/**
+	 * Get an observable food diary entry and it's accompanying {@link
+	 * com.fitnesstracker.database.entities.Food} as a {@link Meal} by ID.
+	 * <p>
+	 * This method will not return null, but the returned {@link LiveData} may contain null.
+	 *
+	 * @param foodDiaryEntryId the ID to search for
+	 *
+	 * @return an observable meal with a matching ID
+	 */
 	@Transaction
 	@Query("SELECT * FROM diary_food WHERE id = :foodDiaryEntryId")
 	public abstract LiveData<Meal> getMealLD(Long foodDiaryEntryId);
 
+	/**
+	 * Get a list of all food diary entries and their accompanying {@link
+	 * com.fitnesstracker.database.entities.Food} objects as {@link Meal} objects.
+	 *
+	 * @return a list of all meals
+	 */
 	@Transaction
 	@Query("SELECT * FROM diary_food ORDER BY time DESC")
 	public abstract List<Meal> getAllMeals();
 
+	/**
+	 * Get an observable list of all food diary entries and their accompanying {@link
+	 * com.fitnesstracker.database.entities.Food} objects as {@link Meal} objects.
+	 *
+	 * @return an observable list of all meals
+	 */
 	@Transaction
 	@Query("SELECT * FROM diary_food ORDER BY time DESC")
 	public abstract LiveData<List<Meal>> getAllMealsLD();
 
 	/**
-	 * Update a {@link FoodDiaryEntry} entity using a {@link Meal}.
+	 * Update one or more {@link FoodDiaryEntry} entities using {@link Meal} objects.
+	 * <p>
+	 * This method may assign a food diary entry a different {@link com.fitnesstracker.database.entities.Food},
+	 * but it will <b>not</b> commit to the database any changes made to food objects.
 	 *
-	 * @param meal the meal whose underlying diary entry should be updated
+	 * @param meals the meals whose underlying diary entries should be updated
 	 *
 	 * @return the number of database rows that were updated
 	 */
-	public int update(Meal meal) {
-		return update(meal.getFoodDiaryEntry());
+	public int update(Meal... meals) {
+		int updates = 0;
+		for (Meal meal : meals) {
+			updates += update(meal.getFoodDiaryEntry());
+		}
+		return updates;
 	}
 }
